e8d93ede30fdab7fd73d805fffaeb87a746b33d1
Krittawat Wisedsuwon <krittawat.code@gmail.com>
2020-09-06 22:06:11 +0700

    add section 7 recusive and error

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # go_relearn
+# folder name convention doesn't apply on this project because I want to structor it like doucument that sort by number 
---
+++ src/section_7_function/73_error/intro.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+	// Best practice
+	/*
+		- not capitalize
+		- avoid newline
+	*/
+
+	// overall
+	/*
+		- propagate error
+		- retry
+		- stop program gracefully
+		- reduced functionalty
+		- ignor error
+	*/
+}
